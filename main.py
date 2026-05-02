02a04105d8f627a18fc8097027220787f702acb6
mcbrt5 <mbartnik95@gmail.com>
2026-05-02 20:20:34 +0200

    feat: add jinja2 templates and bootstrap frontend

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from posts import posts
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]["title"]}</h1>"
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
 
 @app.get("/api/posts")
 def get_posts():
